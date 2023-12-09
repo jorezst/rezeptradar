ddefb0ab8ad14138349b17c0ffd7bcef8bca5f30
jorezst <jrs.stampa@gmail.com>
2023-12-09 11:42:44 +0100

    test for toggle theme button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <template>
   <v-btn> Home </v-btn>
+  <v-btn @click="toggleTheme">toggle theme</v-btn>
 </template>
+
+<script setup>
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+}
+</script>
 
 <script>
 import { defineComponent } from "vue";
