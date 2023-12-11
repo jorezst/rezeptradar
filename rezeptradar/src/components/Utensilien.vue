f3425f74b96a9ac36e77c76c944356f3519bf9da
Timminator97 <timmi97@web.de>
2023-12-11 09:39:44 +0100

    Utensilien Komponente

---
+++ rezeptradar/src/components/Utensilien.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <v-combobox
+      multiple
+      label="Utensilien"
+      :items="['Pfanne groß', 'Pfanne klein', 'Kochtopf groß', 'Kochtopf klein', 'Auflaufform', 'Backblech', 'Knoblauchpresse']"
+  ></v-combobox>
+</template>
+
+<style scoped>
+
+</style>
