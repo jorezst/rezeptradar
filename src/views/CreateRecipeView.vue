78b99649fce1a22ebd6372b732607f303bb48d68
jorezst <jrs.stampa@gmail.com>
2023-12-11 10:02:54 +0100

    Added Utensilien Component

--- src/views/CreateRecipeView.vue
+++ src/views/CreateRecipeView.vue
@@ -1,18 +1,20 @@
 <template>
   <SelectDishType />
   <CreatingSteps />
+  <Utensilien />
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import SelectDishType from "@/components/SelectDishType.vue";
 import CreatingSteps from "@/components/CreatingSteps.vue";
+import Utensilien from "@/components/Utensilien.vue";
 
 // Components
 
 export default defineComponent({
   name: "CreateRecipeView",
 
-  components: { SelectDishType, CreatingSteps },
+  components: {Utensilien, SelectDishType, CreatingSteps },
 });
 </script>
