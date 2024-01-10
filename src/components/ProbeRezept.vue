226f11d8df78fb0209bd3f014a163508c236eaa6
Vinc5555 <107349209+Vinc5555@users.noreply.github.com>
2024-01-10 18:23:15 +0100

    Proberezept mit Pinia erstellt, Testweise auf Homeview ausgegeben

---
+++ src/components/ProbeRezept.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <h1>{{ recipe.title }}</h1>
+      <ul>
+        <li v-for="(ingredient, index) in recipe.ingredients" :key="index">
+          {{ ingredient }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { ref, onMounted } from 'vue';
+  import { useRecipeStore } from '@/stores/ProbeRezept';
+  
+  export default {
+    name: 'RecipeView',
+    setup() {
+      const recipeStore = useRecipeStore();
+      const recipe = ref(recipeStore.recipe);
+  
+      onMounted(() => {
+        // Lade die Rezeptdaten beim Laden der Komponente
+        recipe.value = recipeStore.recipe;
+      });
+  
+      return { recipe };
+    },
+  };
+  </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
   <v-btn>Home</v-btn>
+  <ProbeRezept />
 </template>
 
 <script>
+import ProbeRezept from "@/components/ProbeRezept.vue";
 import { defineComponent } from "vue";
 
 // Components
 
 export default defineComponent({
-  name: "HomeView",
-  components: {},
-});
+    name: "HomeView",
+    components: { ProbeRezept }
+},
+);
 </script>
