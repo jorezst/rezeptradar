9209cbec48e81dfa564cf5beb3be714ffa9a13df
Vinc5555 <107349209+Vinc5555@users.noreply.github.com>
2024-01-10 19:40:51 +0100

    Darstellung angepasst und Datenbasis erweitert

--- src/components/ProbeRezept.vue
+++ src/components/ProbeRezept.vue
@@ -1,12 +1,28 @@
 <template>
+    <v-card class="mx-auto" width="50%">
     <div>
       <h1>{{ recipe.title }}</h1>
+      <h3>Lebensmittel</h3>
       <ul>
         <li v-for="(ingredient, index) in recipe.ingredients" :key="index">
           {{ ingredient }}
         </li>
       </ul>
+      <h3>Utensilien</h3>
+      <ul>
+        <li v-for="(utensilien, index) in recipe.utensilien" :key="index">
+          {{ utensilien }}
+        </li>
+      </ul>
+      <h3>Schritte</h3>
+      <ul>
+        <li v-for="(schritte, index) in recipe.schritte" :key="index">
+          {{ schritte }}
+        </li>
+      </ul>
+      <!--<div>{{ recipe.schritte }}</div>-->
     </div>
+    </v-card>
   </template>
   
   <script>
