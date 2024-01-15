baaa20fa70eb87724bf93be8273109247b54fce2
jorezst <jrs.stampa@gmail.com>
2024-01-15 17:43:56 +0100

    Added a search bar component

---
+++ src/components/SearchBarComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-text-field
+        v-model="searchQuery"
+        label="Rezepte suchen"
+        outlined
+        clearable
+        @input="updateSearch"
+      ></v-text-field>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "SearchBarComponent",
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+  methods: {
+    updateSearch() {
+      this.$emit("search-updated", this.searchQuery);
+    },
+  },
+};
+</script>
