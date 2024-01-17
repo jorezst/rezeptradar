deb6ea8462f1ac0eab8cd10250e6d0efad6fe765
jorezst <jrs.stampa@gmail.com>
2024-01-17 02:05:11 +0100

    changed recipe.js to actual store, to be able to add categories

--- src/store/data/categories.js
+++ src/store/data/categories.js
@@ -1,11 +1,32 @@
-export const categories = [
-  "Frühstück",
-  "Mittagessen",
-  "Abendessen",
-  "Dessert",
-  "Vorspeisen",
-  "Backrezepte",
-  "Vegan",
-  "Vegetarisch",
-  "Lieblingsrezepte",
-];
+import { defineStore } from "pinia";
+
+export const useCategoriesStore = defineStore("categories", {
+  state: () => ({
+    categories: [
+      "Frühstück",
+      "Mittagessen",
+      "Abendessen",
+      "Dessert",
+      "Vorspeisen",
+      "Backrezepte",
+      "Vegan",
+      "Vegetarisch",
+      "Lieblingsrezepte",
+    ],
+  }),
+  actions: {
+    addCategory(category) {
+      if (!this.categories.includes(category)) {
+        this.categories.push(category);
+      }
+    },
+    removeCategory(category) {
+      this.categories = this.categories.filter((c) => c !== category);
+    },
+  },
+  getters: {
+    getCategory: (state) => (category) => {
+      return state.categories.includes(category);
+    },
+  },
+});
