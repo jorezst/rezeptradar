d5f785c5f9b6bd250088ca23102e0340198f991a
jorezst <jrs.stampa@gmail.com>
2024-01-18 23:33:36 +0100

    Added utensil and step input and fixed every rule for input

--- src/views/CreateRecipeView.vue
+++ src/views/CreateRecipeView.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-form ref="form">
+    <v-form ref="mainForm">
       <v-card variant="tonal" class="rounded-xl mb-4">
         <v-card-title class="mb-1"> Hauptinformationen </v-card-title>
         <v-card-item>
@@ -46,7 +46,8 @@
           ></v-select>
         </v-card-item>
       </v-card>
-
+    </v-form>
+    <v-form ref="ingredientForm">
       <v-card variant="tonal" class="rounded-xl mb-4">
         <v-card-title class="mb-1"> Zutaten </v-card-title>
         <v-card-item>
@@ -85,34 +86,72 @@
               ></v-select>
             </v-col>
           </v-row>
-          <v-btn block class="rounded-xl" color="primary" @click="addIngredient"
-            >Add Ingredient</v-btn
+          <v-btn
+            block
+            class="rounded-xl mb-2"
+            color="primary"
+            @click="addIngredient"
+            >Zutat hinzufügen</v-btn
           >
         </v-card-item>
       </v-card>
-
+    </v-form>
+    <v-form ref="utensilForm">
       <v-card variant="tonal" class="rounded-xl mb-4">
-        <v-card-title class="mb-1"> Utensilien </v-card-title>
+        <v-card-title class="mb-1">Utensilien</v-card-title>
         <v-card-item>
-          <div v-for="(utensil, index) in recipe.utensils" :key="index">
-            <v-text-field
-              density="comfortable"
-              rounded
-              label="Utensil"
-              v-model="utensil.name"
-              :rules="utensilNameRules"
-              variant="outlined"
-              clearable
-            ></v-text-field>
-            <v-btn icon @click="removeUtensil(index)">
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-          </div>
-          <v-btn @click="addUtensil">Add Utensil</v-btn>
+          <v-text-field
+            density="comfortable"
+            rounded
+            label="Utensil"
+            v-model="newUtensil"
+            :rules="utensilNameRules"
+            variant="outlined"
+            clearable
+          ></v-text-field>
+          <v-btn
+            block
+            class="rounded-xl mb-2"
+            color="primary"
+            @click="addUtensil"
+          >
+            Utensil hinzufügen
+          </v-btn>
         </v-card-item>
       </v-card>
-      <v-btn block color="primary" @click="submitForm">Submit</v-btn>
     </v-form>
+    <v-form ref="stepForm">
+      <v-card variant="tonal" class="rounded-xl mb-4">
+        <v-card-title class="mb-1">Schritte</v-card-title>
+        <v-card-item>
+          <v-textarea
+            label="Schritt"
+            v-model="newStep.description"
+            :rules="stepRules"
+            variant="outlined"
+            clearable
+            textarea
+            rows="3"
+            auto-grow
+          ></v-textarea>
+          <v-text-field
+            density="comfortable"
+            rounded
+            label="Link zum Bild"
+            v-model="newStep.image"
+            :rules="stepImageRules"
+            variant="outlined"
+            clearable
+          ></v-text-field>
+          <v-btn block class="rounded-xl mb-2" color="primary" @click="addStep">
+            Schritt hinzufügen
+          </v-btn>
+        </v-card-item>
+      </v-card>
+    </v-form>
+    <v-btn block color="primary" class="rounded-xl mb-2" @click="submitForm"
+      >Rezept hinzufügen</v-btn
+    >
   </v-container>
 </template>
 
@@ -121,25 +160,26 @@ import { ref } from "vue";
 import { useRecipesStore } from "@/store/data/recipes";
 import { useCategoriesStore } from "@/store/data/categories.js";
 import { units } from "@/store/data/units.js";
-
-// Components
-
 export default {
   setup() {
     const categoriesStore = useCategoriesStore();
     const recipesStore = useRecipesStore();
-    const form = ref(null);
+    const mainForm = ref(null);
+    const ingredientForm = ref(null);
+    const utensilForm = ref(null);
+    const stepForm = ref(null);
     const recipe = ref({
       name: "",
       mainImage: "",
       time: "",
-      ingredients: [{ name: "", amount: "", unit: "" }],
+      ingredients: [],
       utensils: [],
-      steps: [{ description: "", image: "" }],
+      steps: [],
     });
 
     const newIngredient = ref({ name: "", amount: "", unit: "" });
-    const newUtensil = ref([""]);
+    const newUtensil = ref("");
+    const newStep = ref({ description: "", image: "" });
 
     const nameRules = [
       (v) => !!v || "Name ist erforderlich",
@@ -196,17 +236,42 @@ export default {
     const utensilNameRules = [
       (v) => !!v || "Utensilienname ist erforderlich",
       (v) =>
-        (v && v.length <= 30) ||
-        "Utensilienname darf nicht länger als 30 Zeichen sein",
+        (v && v.length <= 50) ||
+        "Utensilienname darf nicht länger als 50 Zeichen sein",
       (v) =>
         (v && v.length >= 3) ||
         "Utensilienname muss mindestens 3 Zeichen lang sein",
     ];
 
+    const stepRules = [
+      (v) => !!v || "Schrittbeschreibung ist erforderlich",
+      (v) =>
+        (v && v.length <= 600) ||
+        "Schritt darf nicht länger als 600 Zeichen sein",
+      (v) =>
+        (v && v.length >= 10) ||
+        "Schritt darf nicht kürzer als 10 Zeichen sein",
+    ];
+
+    const stepImageRules = [
+      (v) => !!v || "Bild ist erforderlich",
+      (v) => {
+        const pattern =
+          /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
+        return pattern.test(v) || "Falsche URL";
+      },
+    ];
+
     // Methods
-    const addIngredient = () => {
-      recipe.value.ingredients.push({ ...newIngredient.value });
-      resetIngredientForm();
+    const addIngredient = async () => {
+      const validationResult = await ingredientForm.value.validate();
+      if (validationResult.valid) {
+        // Add the new ingredient
+        recipe.value.ingredients.push(newIngredient.value);
+        resetIngredientForm();
+      } else {
+        console.log("Ingredient fields are not correctly filled");
+      }
     };
 
     const resetIngredientForm = () => {
@@ -217,9 +282,14 @@ export default {
       recipe.value.ingredients.splice(index, 1);
     };
 
-    const addUtensil = () => {
-      recipe.value.utensils.push({ ...newUtensil.value });
-      resetUtensilForm();
+    const addUtensil = async () => {
+      const validationResult = await utensilForm.value.validate();
+      if (validationResult.valid) {
+        recipe.value.utensils.push(newUtensil.value);
+        resetUtensilForm();
+      } else {
+        console.log("Utensil name is required");
+      }
     };
 
     const resetUtensilForm = () => {
@@ -230,8 +300,18 @@ export default {
       recipe.value.utensils.splice(index, 1);
     };
 
-    const addStep = () => {
-      recipe.value.steps.push({ description: "", image: "" });
+    const addStep = async () => {
+      const validationResult = await stepForm.value.validate();
+      if (validationResult.valid) {
+        recipe.value.steps.push(newStep.value);
+        resetStepForm();
+      } else {
+        console.log("step fields are not correctly filled");
+      }
+    };
+
+    const resetStepForm = () => {
+      newStep.value = { description: "", image: "" };
     };
 
     const removeStep = (index) => {
@@ -239,7 +319,22 @@ export default {
     };
 
     const submitForm = () => {
-      form.value.validate().then((validationResult) => {
+      if (recipe.value.ingredients.length === 0) {
+        console.log("At least one ingredient is required");
+        return;
+      }
+
+      if (recipe.value.utensils.length === 0) {
+        console.log("At least one utensil is required");
+        return;
+      }
+
+      if (recipe.value.steps.length === 0) {
+        console.log("At least one utensil is required");
+        return;
+      }
+
+      mainForm.value.validate().then((validationResult) => {
         if (!validationResult.valid) {
           console.log("Form is invalid", validationResult.errors);
           // Handle invalid form here
@@ -256,6 +351,8 @@ export default {
       categoriesStore,
       units,
       newIngredient,
+      newUtensil,
+      newStep,
       addIngredient,
       resetIngredientForm,
       removeIngredient,
@@ -263,9 +360,13 @@ export default {
       resetUtensilForm,
       removeUtensil,
       addStep,
+      resetStepForm,
       removeStep,
       submitForm,
-      form,
+      mainForm,
+      ingredientForm,
+      utensilForm,
+      stepForm,
       nameRules,
       mainImageRules,
       timeRules,
@@ -274,6 +375,8 @@ export default {
       ingredientAmountRules,
       ingredientUnitRules,
       utensilNameRules,
+      stepRules,
+      stepImageRules,
     };
   },
 };
