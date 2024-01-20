431114e15a309bc4ec5fd8b4df2c792238468bc3
jorezst <jrs.stampa@gmail.com>
2024-01-20 21:47:19 +0100

    Added a new categoryfilter component to filter by categories

---
+++ src/components/home/CategoryFilter.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-select
+        rounded
+        color="primary"
+        variant="outlined"
+        :items="categories"
+        label="Kategorie auswählen"
+        v-model="selectedCategory"
+        clearable
+      ></v-select>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+import { useCategoriesStore } from "@/store/data/categories.js";
+export default {
+  name: "CategoryFilter",
+  emits: ["update-category"],
+  setup(props, { emit }) {
+    const { categories } = useCategoriesStore();
+    const selectedCategory = ref("");
+
+    // Watch the selectedCategory for changes
+    watch(selectedCategory, (newValue) => {
+      emit("update-category", newValue);
+    });
+
+    return {
+      categories,
+      selectedCategory,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
