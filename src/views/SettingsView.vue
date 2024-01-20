e42fdc28c05a84cadc214c89f2a01ed00ae739bc
jorezst <jrs.stampa@gmail.com>
2024-01-20 03:32:07 +0100

    Converted settings to composition api

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -10,8 +10,8 @@
       <v-col>
         <v-form @submit.prevent="addCategory">
           <v-text-field
-            label="New Category"
             v-model="newCategory"
+            label="New Category"
             outlined
             required
           ></v-text-field>
@@ -22,32 +22,29 @@
   </v-container>
 </template>
 
-<script setup>
-import { useTheme } from "vuetify";
-
-const theme = useTheme();
-
-function toggleTheme() {
-  theme.global.name.value = theme.global.current.value.dark
-    ? "myCustomLightTheme"
-    : "myCustomDarkTheme";
-}
-</script>
-
 <script>
+import { ref } from "vue";
+import { useTheme } from "vuetify";
 import { useCategoriesStore } from "@/store/data/categories";
+
 export default {
-  data() {
-    return {
-      newCategory: "",
-    };
-  },
-  methods: {
-    addCategory() {
-      const categoriesStore = useCategoriesStore();
-      categoriesStore.addCategory(this.newCategory);
-      this.newCategory = "";
-    },
+  setup() {
+    const theme = useTheme();
+    const categoriesStore = useCategoriesStore();
+    const newCategory = ref("");
+
+    function toggleTheme() {
+      theme.global.name.value = theme.global.current.value.dark
+        ? "myCustomLightTheme"
+        : "myCustomDarkTheme";
+    }
+
+    function addCategory() {
+      categoriesStore.addCategory(newCategory.value);
+      newCategory.value = "";
+    }
+
+    return { newCategory, toggleTheme, addCategory };
   },
 };
 </script>
